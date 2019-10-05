cc81400cad17d1a6f4384ecf18eb5dbe56b9e115
Your Name <you@example.com>
2019-10-05 16:27:23 -0300

    some refactoring

--- api.py
+++ api.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 from twisted.web.resource import Resource, NoResource
-from json import dumps
+from json import dumps, loads
+from logging import debug, info, warning, error
+
 
 class API(Resource):
   isLeaf = True
@@ -10,22 +12,62 @@ class API(Resource):
     super().__init__()
     self.konnect = konnect
 
+  def render(self, request):
+    request.setHeader("Content-Type", "application/json")
+    address = request.getClientAddress()
+    self.client = "{}:{}".format(address.host, address.port)
+    info("%s - %s %s", self.client, request.method.decode(), request.uri.decode())
+
+    return super().render(request)
+
   def render_GET(self, request):
     if request.uri.decode() == "/":
-      return b"{}"
-    elif request.uri.decode() == "/devices":
-      return dumps(self.konnect.getDevices())
-    elif request.uri.decode() == "/ping":
-      self.konnect.sendPings()
-      return b"{}"
+      pass
+    elif request.uri.decode() == "/device":
+      return dumps(self.konnect.getDevices()).encode()
     else:
       request.setResponseCode(404)
-      return b""
+
+  def render_PUT(self, request):
+    command, identifier = request.uri.decode()[1:].split("/", 1)
+
+    if command == "device":
+      data = loads(request.content.read())
+
+      if "pair" in data:
+        pair = data["pair"]
+        response = {"success": False}
+        result = self.konnect.requestPair(identifier)
+
+        if result == True:
+          response["success"] = True
+        elif result == False:
+          response["message"] = "already paired"
+        elif result is None:
+          response["message"] = "device not reachable"
+
+        return dumps(response).encode()
+      else:
+        request.setResponseCode(400)
+    else:
+      request.setResponseCode(404)
 
   def render_POST(self, request):
-    if request.uri.decode() == "/ping":
-      self.konnect.sendPings()
-      return b"{}"
+    command, identifier = request.uri.decode()[1:].split("/", 1)
+
+    if command == "ping":
+      result = self.konnect.sendPing(identifier)
+      response = {"success": False}
+
+      if result == True:
+        response["success"] = True
+      elif result == False:
+        response["message"] = "device not reachable"
+      elif result is None:
+        response["message"] = "device not paired"
+
+      return dumps(response).encode()
+    elif command == "notify":
+      pass
     else:
       request.setResponseCode(404)
-      return b""
--- konnect.py
+++ konnect.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python3
 
 from twisted.internet import reactor
-from protocols import KonnectFactory, Discovery
 from twisted.web.server import Site
+from logging import basicConfig, DEBUG, INFO, info
+from protocols import KonnectFactory, Discovery
 from api import API
 
 
 if __name__ == "__main__":
+  basicConfig(format="%(asctime)s %(levelname)s %(message)s", level=DEBUG)
+
   identifier = "test"
   device = "Test"
 
   factory = KonnectFactory(identifier)
   reactor.listenTCP(1764, factory)
   reactor.listenUDP(0, Discovery(identifier, device))
-  reactor.listenTCP(8080, Site(API(factory)))
+  reactor.listenTCP(8080, Site(API(factory)), interface="127.0.0.1")
   reactor.run()
