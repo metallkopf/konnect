4ef3a1cfb148fdd9a178c01b34bf221ac397f5d6
metallkopf <47254555+metallkopf@users.noreply.github.com>
2019-10-05 19:17:00 -0300

    some cleaning

--- api.py
+++ api.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-from twisted.web.resource import Resource, NoResource
+from twisted.web.resource import Resource
 from json import dumps, loads
+from json.decoder import JSONDecodeError
 from logging import debug, info, warning, error
 
 
@@ -13,26 +14,41 @@ class API(Resource):
     self.konnect = konnect
 
   def render(self, request):
-    request.setHeader("Content-Type", "application/json")
+    request.setHeader(b"content-type", b"application/json")
     address = request.getClientAddress()
     self.client = "{}:{}".format(address.host, address.port)
-    info("%s - %s %s", self.client, request.method.decode(), request.uri.decode())
+    self.uri = request.uri.decode()
+    info("%s - %s %s", self.client, request.method.decode(), self.uri)
 
     return super().render(request)
 
   def render_GET(self, request):
-    if request.uri.decode() == "/":
-      pass
-    elif request.uri.decode() == "/device":
+    if self.uri == "/":
+      return b""
+    elif self.uri == "/device":
       return dumps(self.konnect.getDevices()).encode()
+    elif self.uri.startswith("/device/"):
+      identifier = self.uri[1:].split("/", 1)[-1]
+      device = self.konnect.getDevices().get(identifier)
+
+      if device is None:
+        request.setResponseCode(404)
+        return b""
+      else:
+        return dumps(device).encode()
     else:
       request.setResponseCode(404)
+      return b""
 
   def render_PUT(self, request):
-    command, identifier = request.uri.decode()[1:].split("/", 1)
+    if self.uri.startswith("/device/"):
+      try:
+        data = loads(request.content.read())
+      except JSONDecodeError:
+        request.setResponseCode(400)
+        return b""
 
-    if command == "device":
-      data = loads(request.content.read())
+      identifier = self.uri[1:].split("/", 1)[-1]
 
       if "pair" in data:
         pair = data["pair"]
@@ -49,13 +65,14 @@ class API(Resource):
         return dumps(response).encode()
       else:
         request.setResponseCode(400)
+        return b""
     else:
       request.setResponseCode(404)
+      return b""
 
   def render_POST(self, request):
-    command, identifier = request.uri.decode()[1:].split("/", 1)
-
-    if command == "ping":
+    if self.uri.startswith("/ping/"):
+      identifier = self.uri[1:].split("/", 1)[-1]
       result = self.konnect.sendPing(identifier)
       response = {"success": False}
 
@@ -67,7 +84,21 @@ class API(Resource):
         response["message"] = "device not paired"
 
       return dumps(response).encode()
-    elif command == "notify":
-      pass
+    elif self.uri.startswith("/notification/"):
+      try:
+        data = loads(request.content.read())
+      except JSONDecodeError:
+        request.setResponseCode(400)
+        return b""
+
+      identifier = self.uri[1:].split("/", 1)[-1]
+
+      if ["text", "title"] not in data:
+        request.setResponseCode(400)
+        return b""
+      else:
+        self.konnect.sendNotification(identifier, data["text"], data["title"], data.get("app"))
+        #
     else:
       request.setResponseCode(404)
+      return b""
--- cert.py
+++ cert.py
@@ -3,30 +3,37 @@
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.serialization import Encoding, PrivateFormat, NoEncryption
 from cryptography.hazmat.primitives.asymmetric.rsa import generate_private_key
-from cryptography.x509 import Name, NameAttribute, CertificateBuilder, SubjectAlternativeName, DNSName
+from cryptography.x509 import Name, NameAttribute, CertificateBuilder
 from cryptography.x509.oid import NameOID
 from cryptography.hazmat.primitives.hashes import SHA256
 from datetime import datetime, timedelta
+from os.path import exists
 
-key = generate_private_key(65537, 2048, default_backend())
 
-with open("privatekey.pem", "wb") as f:
-    f.write(key.private_bytes(Encoding.PEM, PrivateFormat.TraditionalOpenSSL, NoEncryption()))
+CERT_FILE = "certificate.pem"
+KEY_FILE = "privateKey.pem"
 
-identifier = "d41d8cd98f00b204e9800998ecf8427e"#"b7301ce0878a0ffc"#"_eef31d70_5060_44c1_a4ae_489a46b78790_"
 
-subject = issuer = Name([
+def generate_selfsigned(identifier):
+  if exists(CERT_FILE) and exists(KEY_FILE):
+    return
+
+  key = generate_private_key(65537, 2048, default_backend())
+
+  with open(KEY_FILE, "wb") as pem:
+    pem.write(key.private_bytes(Encoding.PEM, PrivateFormat.TraditionalOpenSSL, NoEncryption()))
+
+  subject = issuer = Name([
     NameAttribute(NameOID.COMMON_NAME, identifier),
     NameAttribute(NameOID.ORGANIZATIONAL_UNIT_NAME, "KDE Connect"),
     NameAttribute(NameOID.ORGANIZATION_NAME, "KDE"),
-])
+  ])
 
-before = datetime.utcnow() - timedelta(days=365)
-after = before + timedelta(days=3650)
+  before = datetime.utcnow() - timedelta(days=365)
+  after = before + timedelta(days=3650)
+  cert = CertificateBuilder().subject_name(subject).issuer_name(issuer).\
+    public_key(key.public_key()).serial_number(1).not_valid_before(before).\
+    not_valid_after(after).sign(key, SHA256(), default_backend())
 
-cert = CertificateBuilder().subject_name(subject).issuer_name(issuer).\
-  public_key(key.public_key()).serial_number(1).not_valid_before(before).\
-  not_valid_after(after).sign(key, SHA256(), default_backend())
-
-with open("certificate.pem", "wb") as f:
-    f.write(cert.public_bytes(Encoding.PEM))
+  with open(CERT_FILE, "wb") as pem:
+    pem.write(cert.public_bytes(Encoding.PEM))
